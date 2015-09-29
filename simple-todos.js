dd6fd2eb2d5da33328b2cb2704326eb195c42de9
duynguyen <nguyenhd2107@gmail.com>
2015-09-29 17:09:11 +0700

    initial commit

---
+++ simple-todos.js
@@ -0,0 +1,51 @@
+Tasks = new Mongo.Collection("tasks");
+ 
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      // Show newest tasks at the top
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var text = event.target.text.value;
+ 
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date(),            // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username  // username of logged in user
+      });
+ 
+      // Clear form
+      event.target.text.value = "";
+    }
+  });
+
+
+  Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function (event, template) {
+      var checkedEle = template.find(".toggle-checked");
+      if(checkedEle.checked)
+        Tasks.remove(this._id);
+    }
+  });
+
+   Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+}
